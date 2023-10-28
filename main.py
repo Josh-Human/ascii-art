bc79c291ca95ebf0447659c45ebee2ed45ee3a1a
Joshua Human <jhumanb1@gmail.com>
2023-10-28 18:43:39 +0100

    feat: add console line arg for brightness algo

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ CHARACTERS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@
 parser = argparse.ArgumentParser(description='Print an ascii art version of an image in the console.')
 parser.add_argument('path', metavar='p', type=str, nargs='?',
                     help='path of image to transform')
+parser.add_argument('--brightness_algorithm', metavar="--b",type=str, required=False, choices=['luminosity', 'min_max', 'average'],
+                    default='average',
+                    help='algorithm to calculate pixel brightness')
 
-def main(img_path):
+def main(img_path, brightness_algorithm):
     img = Image.open(img_path)
 
     print("Succesfully loaded image!")
-    
+
     resalce_factor = 0.25
     img.resize((round(img.width * resalce_factor), round(img.height * resalce_factor)))
 
@@ -22,13 +25,14 @@ def main(img_path):
     pixel_matrix = construct_empty_matrix(img.height, img.width)
     brightness_matrix = construct_empty_matrix(img.height, img.width)
     character_matrix = construct_empty_matrix(img.height, img.width)
+
     for y in range(img.height):
         for x in range(img.width):
             pixel_matrix[x][y] = img.getpixel((x, y))
 
             pixel = pixel_matrix[x][y]
 
-            brightness_matrix[x][y] = calculate_brightness(pixel, "luminosity")
+            brightness_matrix[x][y] = calculate_brightness(pixel, brightness_algorithm)
             character_matrix[x][y] = convert_brightness_to_charcter(brightness_matrix[x][y])
             print(character_matrix[x][y], end="")
         print("")
@@ -42,14 +46,14 @@ def convert_brightness_to_charcter(brightness):
     return CHARACTERS[position]
 
 def calculate_brightness(pixel, algo=None):
-
-    if algo == "lightness":
-        return round((max(pixel[0] + pixel[1] + pixel[2]) + min(pixel[0] + pixel[1] + pixel[2])) / 2)
+    if algo == "min_max":
+        return round((max(pixel[0], pixel[1], pixel[2]) + min(pixel[0], pixel[1] , pixel[2])) / 2)
     elif algo == "luminosity":
-        return round(0.21*pixel[0] + 0.72*pixel[1] + 0.07*pixel[2])
+        return round(0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2])
     else:
         return round((pixel[0] + pixel[1] + pixel[2]) / 3)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(args.path)
+    print(args)
+    main(args.path, args.brightness_algorithm)
